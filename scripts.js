309d5df98b111c3d5eb2e368a03256ec7d36512c
MEDIAART-3BB3 <mediaart3bb3@gmail.com>
2024-12-02 09:05:39 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Navigation logic
   const links = [
     'index.html',
-    'hamilton_future.html',
+    // 'hamilton_future.html',
     'elysium.html',
     'futureofcars.html',
     'futureofchinesearchitecture.html',
